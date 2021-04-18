c0bed27398c22c504e82db6230db27706840dcd3
Giulia Lutke <giulialutke@gmail.com>
2021-04-18 16:54:48 -0300

    Exercicios aula java 18/04/2021

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		imparPar();
+		diadasemana2();
 	}
 
 	public static void exemploDadosDigitados() {
@@ -393,4 +393,105 @@ public class Main {
 
 	}
 
+	// Exemplo calculo minutos
+
+	public static void calculominutos() {
+		Scanner sc = new Scanner(System.in);
+
+		int minutos = sc.nextInt();
+
+		double conta = 50.0;
+		if (minutos > 100) {
+			conta = conta + (minutos - 100) * 2.0;
+		}
+
+		System.out.printf("Valor da conta = R$ %.2f%n", conta);
+		sc.close();
+
+	}
+
+	// Exemplo calculo minutos simplificado
+
+	public static void calculominutoscumulativa() {
+		Scanner sc = new Scanner(System.in);
+
+		int minutos = sc.nextInt();
+
+		double conta = 50.0;
+		if (minutos > 100) {
+			conta += +(minutos - 100) * 2.0;
+		}
+
+		System.out.printf("Valor da conta = R$ %.2f%n", conta);
+		sc.close();
+
+	}
+
+	// exemplo dia da semana
+
+	public static void diadasemana() {
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		String dia;
+		if (x == 1) {
+			dia = "domingo";
+		} else if (x == 2) {
+			dia = "segunda";
+		} else if (x == 3) {
+			dia = "terca";
+		} else if (x == 4) {
+			dia = "quarta";
+		} else if (x == 5) {
+			dia = "quinta";
+		} else if (x == 6) {
+			dia = "sexta";
+		} else if (x == 7) {
+			dia = "sabado";
+		} else {
+			dia = "valor invalido";
+		}
+		System.out.println("Dia da semana: " + dia);
+		sc.close();
+
+	}
+
+	// exemplo dia da semana 2
+
+	public static void diadasemana2() {
+
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		String dia;
+		switch (x) {
+		case 1:
+			dia = "domingo";
+			break;
+		case 2:
+			dia = "segunda";
+			break;
+		case 3:
+			dia = "terca";
+			break;
+		case 4:
+			dia = "quarta";
+			break;
+		case 5:
+			dia = "quinta";
+			break;
+		case 6:
+			dia = "sexta";
+			break;
+		case 7:
+			dia = "sabado";
+			break;
+		default:
+			dia = "valor invalido";
+			break;
+		}
+		System.out.println("Dia da semana: " + dia);
+		sc.close();
+
+	}
 }
