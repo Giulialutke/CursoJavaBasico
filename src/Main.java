2e7228d3bd624afae034e93c3447a3c469d17114
Giulia Lutke <giulialutke@gmail.com>
2021-04-18 17:08:08 -0300

    exercios 2 18/04/2021

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		diadasemana2();
+		desconto2();
 	}
 
 	public static void exemploDadosDigitados() {
@@ -494,4 +494,33 @@ public class Main {
 		sc.close();
 
 	}
+	
+	//Desconto
+	
+	public static void desconto() {
+		Scanner sc = new Scanner(System.in);
+	
+	double preco = 34.5;
+	double desconto;
+	if (preco < 20.0) {
+	desconto = preco * 0.1;
+	}
+	else {
+	desconto = preco * 0.05;
+	}
+	System.out.println(desconto);
+	sc.close();
+	}
+	
+	//Desconto condicional ternaria
+	
+	public static void desconto2() {
+		Scanner sc = new Scanner(System.in);
+		
+		double preco = 34.5;
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+		System.out.println(desconto);
+		sc.close();
+	}	
+	
 }
