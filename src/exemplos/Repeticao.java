cdab3d0969029a562c98e81b1a58c70c7001c277
Giulialutke <giulialutke@gmail.com>
2021-11-09 22:07:30 -0300

    While

--- src/exemplos/Repeticao.java
+++ src/exemplos/Repeticao.java
@@ -3,9 +3,13 @@ package exemplos;
 import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * @author juh_l
+ *
+ */
 public class Repeticao {
 	public static void main(String[] args) {
-		contagemregressiva();
+		codigo();
 	}
 
 	public static void enquanto() {
@@ -38,18 +42,59 @@ public class Repeticao {
 		System.out.println(soma);
 		sc.close();
 	}
-	
+
 	public static void para2() {
-		
-		for( int i = 0; i<5; i++) {
+
+		for (int i = 0; i < 5; i++) {
 			System.out.println("Valor de i :" + i);
 		}
 	}
 
-		public static void contagemregressiva() {
-			
-			for( int i = 4; i>=0; i--) {
-				System.out.println("Valor de i :" + i);
-			}
+	public static void contagemregressiva() {
+
+		for (int i = 4; i >= 0; i--) {
+			System.out.println("Valor de i :" + i);
 		}
+
 	}
+
+	public static void senhainvalida() {
+		Scanner sc = new Scanner(System.in);
+
+		int senha = sc.nextInt();
+
+		while (senha != 2002) {
+			System.out.println("Senha invalida");
+			senha = sc.nextInt();
+		}
+		System.out.println(" Acesso permitido");
+		sc.close();
+
+	}
+
+	public static void codigo() {
+		Scanner sc = new Scanner(System.in);
+
+		int opc = sc.nextInt();
+		int a = 0;
+		int g = 0;
+		int d = 0;
+
+		while (opc != 4) {
+
+			if (opc == 1) {
+				a = a + 1;
+			} else if (opc == 2) {
+				 g = g + 1;
+			} else if (opc == 3) {
+				d = d + 1 ;
+			}
+			opc = sc.nextInt();
+		}
+		System.out.println("Muito Obrigado");
+		System.out.println("Alcool: " + a);
+		System.out.println("Gasolina: " + g);
+        System.out.println("Diesel: " + d );
+		sc.close();
+	}
+}
