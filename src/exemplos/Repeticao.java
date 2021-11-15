fdac3a28320f3ab378427eed747f1a235f6a94c9
Giulialutke <giulialutke@gmail.com>
2021-11-15 15:09:35 -0300

    split

--- src/exemplos/Repeticao.java
+++ src/exemplos/Repeticao.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  */
 public class Repeticao {
 	public static void main(String[] args) {
-		Formatacao();
+		split();
 	}
 
 	public static void enquanto() {
@@ -213,34 +213,44 @@ public class Repeticao {
 		}
 		sc.close();
 	}
-	
+
 	public static void Formatacao() {
-		
+
 		String original = "abcde FGHIJ ABC abc DEFG ";
-		
+
 		String s01 = original.toLowerCase();
 		String s02 = original.toUpperCase();
-		String s03 = original.trim();//remove espaço do traço
-        String s04 = original.substring(2);
-        String s05 = original.substring(2, 9);
-        String s06 = original.replace('a', 'x');
-        String s07 = original.replace("abc", "xy");
-        
-        int i = original.indexOf("bc");
-        int j = original.lastIndexOf("bc");
-        
-		
-		System.out.println("Original : " + original +  "-");
-		System.out.println("toLowerCase : " + s01 +  "-");
-		System.out.println("toUpperCase : " + s02 +  "-");
-		System.out.println("trim : " + s03 +   "-");
-		System.out.println("substring : " +  s04 + "-");
-		System.out.println("substring : " +  s05 + "-");
-		System.out.println("replace : " +  s06 + "-");
-		System.out.println("replace : " +  s07 + "-");
+		String s03 = original.trim();// remove espaço do traço
+		String s04 = original.substring(2);
+		String s05 = original.substring(2, 9);
+		String s06 = original.replace('a', 'x');
+		String s07 = original.replace("abc", "xy");
+
+		int i = original.indexOf("bc");
+		int j = original.lastIndexOf("bc");
+
+		System.out.println("Original : " + original + "-");
+		System.out.println("toLowerCase : " + s01 + "-");
+		System.out.println("toUpperCase : " + s02 + "-");
+		System.out.println("trim : " + s03 + "-");
+		System.out.println("substring : " + s04 + "-");
+		System.out.println("substring : " + s05 + "-");
+		System.out.println("replace : " + s06 + "-");
+		System.out.println("replace : " + s07 + "-");
 		System.out.println("indexOf 'bc': " + i);
 		System.out.println("LastIndexOf 'bc': " + j);
-		
+
+	}
+
+	public static void split() {
+
+		String s = "potato apple lemon ";
+		String[] vect = s.split(" ");
+
+			
+		System.out.println(vect[0]);
+		System.out.println(vect[1]);
+		System.out.println(vect[2]);
+
 	}
 }
-
